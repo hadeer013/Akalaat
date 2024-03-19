befac1a4492d62e3864eee32a1762fcdc54443ed
Fatma Sabry <71177218+Fatma-Sabry@users.noreply.github.com>
2024-03-19 17:02:54 +0200

    to fetch

--- src/Akalaat/Akalaat/obj/Debug/net8.0/Akalaat.AssemblyInfo.cs
+++ src/Akalaat/Akalaat/obj/Debug/net8.0/Akalaat.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Akalaat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b55cbbf9d11f27b4d1c706d797e3973667054619")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fb227ed605a0c7649a1340a5466afbf98baadf")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Akalaat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Akalaat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
